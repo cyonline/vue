375c882d10095a074cea17093c2a9a41fae6210f
chengyu <chengyuonline@163.com>
2019-06-30 19:15:53 +0800

    2

---
+++ src/components/Container.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        
+    </div>
+</template>
+<script>
+export default {
+    name:'cy-container',
+    data(){
+
+    },
+}
+</script>
+<style lang="scss" scoped>
+    .container{
+        height: calc(100vh - 70px);
+    }
+</style>
--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,11 +1,18 @@
 <template>
-    <div id="content">
-        <input type="text" v-model="name">{{name}}
+    <div class="content">
+        <div>
+            从父组件拿到的数据: prop:{{data}}
+        </div>
+        <div>
+        <h4>表单元素</h4>
+        <!-- <input type="text" v-model="name">{{name}}
         <input type="text" v-model="age">{{age}}
-        <input type="text" v-model="sex">{{sex}}
-        <select v-model="type">
-            <option value="item.id" v-for="item in option" :key="item.id">{{item.name}}</option>
+        <input type="text" v-model="sex">{{sex}} -->
+        <select v-model="type"  @change="selectChange($event)">
+            <option :value="item.id" v-for="item in option" :key="item.id">{{item.name}}</option>
         </select>
+        select:{{typeName}}
+        </div>
     </div>
 </template>
 <script>
@@ -17,15 +24,17 @@ export default {
             age:'',
             sex:'',
             type:'0',
+            typeName:'路飞',
             typeOption:[]
         }
     },
+    props:['data'],
     computed: {
         option:function(){
         let arr = [
-                    {'id':'0',name:'luffy','sex':'0'},
-                    {'id':'1',name:'zoro','sex':'0'},
-                    {'id':'2',name:'nami','sex':'1'},
+                    {'id':'0',name:'路飞','sex':'0'},
+                    {'id':'1',name:'索隆','sex':'0'},
+                    {'id':'2',name:'娜美','sex':'1'},
             ]
             arr.forEach(ele => {
                 ele['group'] = 'caomao';
@@ -35,7 +44,28 @@ export default {
     },
     mounted() {
         console.info('mounted');
+        console.info(this.data);
+        this.$watch('type',function(n,o){
+            console.info(n,o);
+        })
+    },
+    methods: {
+        selectChange(e){
+            console.log(e.target.value);
+            this.option.forEach(ele=>{
+                if(e.target.value === ele.id){
+                    this.typeName = ele.name
+                }
+            })
+        }
     },
 }
 </script>
-
+<style lang="scss">
+    .content{
+        height:calc(100vh - 70px);
+        box-sizing: border-box;
+        padding:10px;
+        background:#cdcdcd;
+    }
+</style>
