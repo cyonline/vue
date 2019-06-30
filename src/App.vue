0ea9800bcde406136d7871a1d5ea309f1d0979e1
chengyu <chengyuonline@163.com>
2019-06-30 15:43:37 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
+    <cy-header></cy-header>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <cy-content></cy-content>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+import cyHeader from './components/Header.vue';
+import cyContent from './components/Content.vue';
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    cyHeader,
+    cyContent
   }
 };
 </script>
@@ -23,6 +28,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+  margin:0;
+  padding:0;
 }
 </style>
---
+++ src/components/Content.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="content">
+        <input type="text" v-model="name">{{name}}
+        <input type="text" v-model="age">{{age}}
+        <input type="text" v-model="sex">{{sex}}
+        <select v-model="type">
+            <option value="item.id" v-for="item in option" :key="item.id">{{item.name}}</option>
+        </select>
+    </div>
+</template>
+<script>
+export default {
+    name:'cy-content',
+    data(){
+        return {
+            name:'',
+            age:'',
+            sex:'',
+            type:'0',
+            typeOption:[]
+        }
+    },
+    computed: {
+        option:function(){
+        let arr = [
+                    {'id':'0',name:'luffy','sex':'0'},
+                    {'id':'1',name:'zoro','sex':'0'},
+                    {'id':'2',name:'nami','sex':'1'},
+            ]
+            arr.forEach(ele => {
+                ele['group'] = 'caomao';
+            });
+            return arr;
+        }
+    },
+    mounted() {
+        console.info('mounted');
+    },
+}
+</script>
+
